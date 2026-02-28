3a9aaf6584fff0489e8e1033f48b462762753560
TheAnimeEnthusiast <mritvik02@gmail.com>
2026-02-27 19:50:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+ch = input("Enter a character: ")
+
+if ch == 'a' or ch == 'A':
+    print("It is an alphabet.")
+elif ch == 'b' or ch == 'B':
+    print("It is an alphabet.")
+elif ch == 'c' or ch == 'C':
+    print("It is an alphabet.")
+elif ch == 'd' or ch == 'D':
+    print("It is an alphabet.")
+elif ch == 'e' or ch == 'E':
+    print("It is an alphabet.")
+elif ch == 'f' or ch == 'F':
+    print("It is an alphabet.")
+elif ch == 'g' or ch == 'G':
+    print("It is an alphabet.")
+elif ch == 'h' or ch == 'H':
+    print("It is an alphabet.")
+elif ch == 'i' or ch == 'I':
+    print("It is an alphabet.")
+elif ch == 'j' or ch == 'J':
+    print("It is an alphabet.")
+elif ch == 'k' or ch == 'K':
+    print("It is an alphabet.")
+elif ch == 'l' or ch == 'L':
+    print("It is an alphabet.")
+elif ch == 'm' or ch == 'M':
+    print("It is an alphabet.")
+elif ch == 'n' or ch == 'N':
+    print("It is an alphabet.")
+elif ch == 'o' or ch == 'O':
+    print("It is an alphabet.")
+elif ch == 'p' or ch == 'P':
+    print("It is an alphabet.")
+elif ch == 'q' or ch == 'Q':
+    print("It is an alphabet.")
+elif ch == 'r' or ch == 'R':
+    print("It is an alphabet.")
+elif ch == 's' or ch == 'S':
+    print("It is an alphabet.")
+elif ch == 't' or ch == 'T':
+    print("It is an alphabet.")
+elif ch == 'u' or ch == 'U':
+    print("It is an alphabet.")
+elif ch == 'v' or ch == 'V':
+    print("It is an alphabet.")
+elif ch == 'w' or ch == 'W':
+    print("It is an alphabet.")
+elif ch == 'x' or ch == 'X':
+    print("It is an alphabet.")
+elif ch == 'y' or ch == 'Y':
+    print("It is an alphabet.")
+elif ch == 'z' or ch == 'Z':
+    print("It is an alphabet.")
+else:
+    print("It is NOT an alphabet.")
